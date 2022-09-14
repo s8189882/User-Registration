35885086b3b2077e0d92fe78fa9f6111363d88cb
s8189882 <s8189882@gmail.com>
2022-09-14 17:03:33 +0530

    Create main branch for validating user entries in user registration system

---
+++ RegEx User Registration/src/regex/user/registration/validation/UserValidation.java
@@ -0,0 +1,8 @@
+package regex.user.registration.validation;
+
+public class UserValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Regular Expressions program to validate user entries in a User Registration System!\n");
+	}
+}
