7d9263b0795e482fe59da24bfa17da1f185c3597
s8189882 <s8189882@gmail.com>
2022-09-14 22:50:02 +0530

    Add UC10 - Validate user input using JUnit Tests

--- RegEx User Registration/src/regex/user/registration/validation/UserValidation.java
+++ RegEx User Registration/src/regex/user/registration/validation/UserValidation.java
@@ -30,11 +30,12 @@ public class UserValidation {
 		validatePassword(getUserInput());
 	}
 	
-	private void validatePassword(String input) {
-		String password = "^(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,}$";
+	public boolean validatePassword(String input) {
+		
+		String password = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";		
 		
 		System.out.println("\nValidating Password...");
-		match(password, input);
+		return match(password, input);
 	}
 
 	public String getUserInput() {
@@ -43,35 +44,37 @@ public class UserValidation {
 		return sc.nextLine();
 	}
 	
-	public void validateMobileNumber(String input) {
+	public boolean validateMobileNumber(String input) {
 		
 		String mobile = "^[0-9]{2}\\s[0-9]{10}$";
 		
 		System.out.println("\nValidating Mobile Number...");
-		match(mobile, input);
+		return match(mobile, input);
 	}
 	
-	public void validateEmail(String input) {
+	public boolean validateEmail(String input) {
 		
 		String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
 		
 		System.out.println("\nValidating Email...");
-		match(email, input);
+		return match(email, input);
 	}
 	
-	public void match(String regex, String input) {
+	public boolean match(String regex, String input) {
 		Pattern pattern = Pattern.compile(regex);
 		Matcher match = pattern.matcher(input);
 		
-		printResult(match.matches(), input);
+		boolean result = (match.matches());
+		printResult(result, input);
+		return result;
 	}
 	
-	public void validateName(String input) {
+	public boolean validateName(String input) {
 		
 		String name = "^[A-Z][a-zA-Z]{2,}$";
 		
 		System.out.println("\nValidating Name...");
-		match(name, input);
+		return match(name, input);
 	}
 	
 	public void printResult(boolean isOkay, String input) {
