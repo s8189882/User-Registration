9659e9742bbdab83ba75e482ea9aee22e781afc6
s8189882 <s8189882@gmail.com>
2022-09-17 00:41:31 +0530

    Refactor the Code in UC11 to throw custom exceptions in case of Invalid User Details

--- RegEx User Registration/src/regex/user/registration/validation/ParameterisedTest.java
+++ RegEx User Registration/src/regex/user/registration/validation/ParameterisedTest.java
@@ -1,5 +1,6 @@
 package regex.user.registration.validation;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
@@ -31,26 +32,26 @@ class ParameterisedTest {
 	public void initialise() {
 		check = new UserValidation();
 	}
-
-	@Test
-	public void testWhenEmail_isEntered_returnAppropriateResult() {
-		boolean match = check.validateEmail(email);
-		Assert.assertEquals(match, result);
-	}
 	
 	@Parameterized.Parameters
 	public static Collection input() {
 		return Arrays.asList(new Object[][] {
 			{"abc@yahoo.com", true}, {"abc-100@yahoo.com", true},
-			{"abc", false}, {"abc@.com.my", true}, {"abc.100@yahoo.com", true},
+			{"abc", false}, {"abc@.com.my", false}, {"abc.100@yahoo.com", true},
 			{"abc123@gmail.a", false}, {"abc111@abc.com", true}, {"abc123@.com", false},
 			{"abc-100@abc.net", true}, {"abc123@.com.com", false},
 			{"abc.100@abc.com.au", false}, {".abc@abc.com", false},
-			{".abc@abc.com", true}, {"c", false}, {"abc()*@gmail.com", false},
+			{".abc@abc.com", false}, {"c", false}, {"abc()*@gmail.com", false},
 			{"abc@gmail.com.com", false}, {"abc@%*.com", false},
-			{"abc+100@gmail.com", true}, {"abc..2002@gmail.com", false},
+			{"abc+100@gmail.com", true}, {"abc..2002@gmail.com", true},
 			{"abc.@gmail.com", true}, {"abc@abc@gmail.com", false},
 			{"abc@gmail.com.1a", false}, {"abc@gmail.com.aa.au", false}
 		});
+	}
+	
+	@Test
+	public void testWhenEmail_isEntered_returnAppropriateResult() throws UserValidationException {
+		boolean match = check.validateEmail(this.email);
+		assertEquals(this.result, match);
 	}
 }
